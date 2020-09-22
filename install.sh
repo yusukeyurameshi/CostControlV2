7329a570b9c486dbdc9ce18a5a14529f4856ff58
yusukeyurameshi <fabio@yusuketi.com>
2020-09-22 15:02:10 -0300

    .

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 CRON="0 10 * * * timeout 6h /home/opc/CostControlV2/shell_scripts/run_daily.sh"
 
 sudo yum install -y git
-sudo pip3 install oci oci-cli requests
+sudo pip3 install oci oci-cli requests pandas
 
 git clone https://github.com/yusukeyurameshi/CostControlV2.git
 
