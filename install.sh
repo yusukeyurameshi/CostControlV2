773d4818d9e6a192d8bf285d3b7bea3953eb6ba9
yusukeyurameshi <fabio@yusuketi.com>
2020-09-22 14:54:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ CRON="0 10 * * * timeout 6h /home/opc/CostControlV2/shell_scripts/run_daily.sh"
 
 sudo yum install -y git
 sudo pip3 install oci oci-cli requests
+
+git clone https://github.com/yusukeyurameshi/CostControlV2.git
