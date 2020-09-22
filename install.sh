aba6ae4456a78b8dd06f6b339ac15292ce829528
yusukeyurameshi <fabio@yusuketi.com>
2020-09-22 17:34:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo pip3 install oci oci-cli requests pandas
 
 git clone https://github.com/yusukeyurameshi/CostControlV2.git
 
-cat crontab.txt | crontab -
+cat /home/opc/CostControlV2/crontab.txt | crontab -
 
 /home/opc/CostControlV2/shell_scripts/run_daily.sh
 
