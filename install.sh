ca61457de32bf2a0e2008044c318f466529d3293
yusukeyurameshi <fabio@yusuketi.com>
2021-01-21 11:57:16 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum install -y git
+sudo yum install -y git python3
 sudo pip3 install oci oci-cli requests pandas
 
 git clone https://github.com/yusukeyurameshi/CostControlV2.git
