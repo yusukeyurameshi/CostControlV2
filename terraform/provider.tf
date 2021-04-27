e0c5e557745d5dfbf4ff9207e99a65891a29e72b
yusukeyurameshi <fabio@yusuketi.com>
2021-04-27 15:16:29 -0300

    Update terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,17 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = ">= 0.14"
 }
+
+provider "oci" {
+  alias                = "homeregion"
+#  tenancy_ocid         = var.tenancy_ocid
+#  user_ocid            = var.user_ocid
+#  fingerprint          = var.fingerprint
+#  private_key_path     = var.private_key_path
+  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
+  disable_auto_retries = "true"
+}
+
 
 # Required by the OCI Provider
 variable compartment_ocid {}
