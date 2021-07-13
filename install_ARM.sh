c1ef02d8725b0d20c054b79c0973408870b763fa
Oracle Public Cloud User <opc@a1.sub07121959010.vcn.oraclevcn.com>
2021-07-13 17:08:47 +0000

    .

--- install_ARM.sh
+++ install_ARM.sh
@@ -2,7 +2,9 @@
 
 sudo yum install -y git python3 platform-python-devel.aarch64
 
-sudo pip3 install oci oci-cli requests pandas numpy cython
+sudo pip3 install cython
+sudo pip3 install numpy
+sudo pip3 install oci oci-cli requests pandas
 
 git clone https://github.com/yusukeyurameshi/CostControlV2.git
 
